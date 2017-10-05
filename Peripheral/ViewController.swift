22410e4be35ec887532e1f45401d96cb433b8816
RTC01 <rtc01@rtc01de-imac.local>
2017-10-05 14:23:05 +0800

    Initial Commit

---
+++ Peripheral/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Peripheral
+//
+//  Created by RTC01 on 2017/10/5.
+//  Copyright © 2017年 JasonHuang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
